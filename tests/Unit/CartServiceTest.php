0a17882108538996129b0c3e0e28f424941ad5f3
Matteo <matteo.marcole3@etu.univ-lorraine.fr>
2026-09-22 15:08:58 +0200

    Test: CartServiceTest.php + CS fixer corrigé

--- tests/Unit/CartServiceTest.php
+++ tests/Unit/CartServiceTest.php
@@ -47,7 +47,8 @@ class CartServiceTest extends TestCase
         $this->assertSame(25.00, $this->service->getTotal($cart));
     }
 
-    public function testMultipleItems(): void {
+    public function testMultipleItems(): void 
+    {
         $user = $this->createStub(User::class);
         $cart = new Cart($user);
 
@@ -72,7 +73,8 @@ class CartServiceTest extends TestCase
         $this->assertSame(59.00, $this->service->getTotal($cart));
     }
 
-    public function testQuantityMultiplier(): void {
+    public function testQuantityMultiplier(): void 
+    {
         $user = $this->createStub(User::class);
         $cart = new Cart($user);
 
